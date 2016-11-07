f4a2c0e1181cb025e2d6007c9a67702b04f0d98f
Jeff Duke <jeff.duke@gmail.com>
2016-11-06 18:31:54 -0700

    refactor microcontroller code to have it post data to sparkfun cloud services

---
+++ phant.js
@@ -0,0 +1,5 @@
+export const phant = {
+  publicUrl: 'http://data.sparkfun.com/streams/ZG4drKRxMocl6a5L7WrY',
+  publicKey: 'ZG4drKRxMocl6a5L7WrY',
+  loggingUrl: 'http://data.sparkfun.com/input/${publicKey}?private_key=${privateKey}&humidity=[value]&moisturelevel=[value]&pressure=[value]&temp=[value]&waterlevel=[value]',
+};
