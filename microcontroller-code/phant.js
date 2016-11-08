b9f28a442337e8b8dbabd6bfc1ed489e648bc28a
Jeff-Duke <Jeff.Duke@Gmail.com>
2016-11-07 19:15:02 -0700

    Delete phant.js

--- microcontroller-code/phant.js
+++
@@ -1,6 +0,0 @@
-export const phant = {
-  publicUrl: 'http://data.sparkfun.com/streams/ZG4drKRxMocl6a5L7WrY',
-  publicKey: 'ZG4drKRxMocl6a5L7WrY',
-  privateKey: '2mvZePN5kMu7y0lArPRZ',
-  loggingUrl: `http://data.sparkfun.com/input/${publicKey}?private_key=${privateKey}&humidity=${humidity}&moisturelevel=${waterLevel}&pressure=${barometer}&temp=${temperature}&waterstatus=${waterStatus}`,
-};
