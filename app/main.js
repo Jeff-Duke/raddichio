4b9cd169723d665b8fb33eb67b9dc5cf7997f5c5
Jeff Duke <jeff.duke@gmail.com>
2016-11-10 08:37:30 -0700

    add styling for the local controls page

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 'use strict';
-window.onload = function() {
+window.onload = () => {
   const socket = io();
   let monitor = {};
 
@@ -10,8 +10,11 @@ window.onload = function() {
     max: 1000,
     title: "WaterLevel",
     relativeGaugeSize: true,
+    labelFontColor: "#FFF",
+    valueFontColor: '#3BFFAB',
+    titleFontColor: '#3ADEE8',
   });
-  
+
   monitor.thermometer = new JustGage({
     id: "thermometer",
     value: 60,
@@ -20,6 +23,9 @@ window.onload = function() {
     title: "Thermometer",
     label: "° Fahrenheit",
     relativeGaugeSize: true,
+    labelFontColor: "#FFF",
+    valueFontColor: '#3BFFAB',
+    titleFontColor: '#3ADEE8',
   });
 
   monitor.barometer = new JustGage({
@@ -30,6 +36,9 @@ window.onload = function() {
     title: "Barometer",
     label: "Pressure/kPa",
     relativeGaugeSize: true,
+    labelFontColor: "#FFF",
+    valueFontColor: '#3BFFAB',
+    titleFontColor: '#3ADEE8',
   });
 
   monitor.hygrometer = new JustGage({
@@ -40,19 +49,28 @@ window.onload = function() {
     title: "Hygrometer",
     label: "Humidity %",
     relativeGaugeSize: true,
+    labelFontColor: "#FFF",
+    valueFontColor: '#3BFFAB',
+    titleFontColor: '#3ADEE8',
   });
-  
-  var displays = Object.keys(monitor);
 
-  socket.on("report", function (data) {
-    displays.forEach(function (display) {
+  const waterOnButton = document.getElementById('water-on-button');
+  const waterOffButton = document.getElementById('water-off-button');
+  const waterStatus = document.getElementById('water-status');
+  let displays = Object.keys(monitor);
+
+  socket.on('report', (data) => {
+    displays.forEach((display) => {
       monitor[display].refresh(data[display]);
     });
   });
-  
-  const waterOnButton = document.getElementById('water-on-button');
-  const waterOffButton = document.getElementById('water-off-button');
-  
+
+  socket.on('waterStatusUpdate', (data) => {
+    waterStatus.innerText = `Watering Status: ${data.waterstatus}`;
+  });
+
+
+
   waterOnButton.addEventListener('click', (e) => {
     socket.emit('waterOnClick');
     e.preventDefault();
